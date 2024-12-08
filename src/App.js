e01c49a268ad86b4d31e0b8e5cada4370aea06cf
mac <mac@macs-MacBook-Pro.local>
2024-12-08 18:58:21 +0530

    added comments

--- src/App.js
+++ src/App.js
@@ -4,23 +4,36 @@ import AddIcon from '@mui/icons-material/Add';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 
 const App = () => {
-  const [Todo, setTodo] = useState([])
-  const [ToDoValue, setTodoValue] = useState('')
+  const [Todo, setTodo] = useState([]) //state for the list of todos  
+  const [ToDoValue, setTodoValue] = useState('')  // state for current value of the input field
 
+
+
+  // function to handle change in input field
   const handleInputChange = (event) => {
+    // update the todovalue with current input value
     setTodoValue(event.target.value)
   }
 
+
+  //function to add a new todo item
   const handleAddTodo = () => {
-    if (ToDoValue.trim() !== '') {
+    //uodate the todo state by adding current todovalue to the existing list
       setTodo([...Todo, ToDoValue]);
+
+      // clear the input by resetting todovalue to empty
       setTodoValue('');
-    }
+    
   };
 
+
+  // function to delete to do item
   const handleDeleteTodo = (key) => {
+    // create a new list by copying the existing todo array
     let newList = [...Todo]
+    //remove the item from specified index
     newList.splice(key, 1)
+    // update the new todo state with the new list after deletation 
     setTodo([...newList])
   }
 
@@ -53,6 +66,7 @@ const App = () => {
         </MenuItem>
       ))}
     </Box>
+    
   )
 }
 
